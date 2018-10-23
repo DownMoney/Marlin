7c7a2469d4a371dcab519d7eea5ca53a84e56d37
Michael <michael@compsci.ninja>
2018-10-23 21:48:47 +0100

    PID tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -355,9 +355,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+//  #define  DEFAULT_Kp 22.2
+//  #define  DEFAULT_Ki 1.08
+//  #define  DEFAULT_Kd 114
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -368,6 +368,11 @@
   //#define  DEFAULT_Kp 63.0
   //#define  DEFAULT_Ki 2.25
   //#define  DEFAULT_Kd 440
+
+  // E3D V6
+  #define  DEFAULT_Kp 12.31
+  #define  DEFAULT_Ki 2.21
+  #define  DEFAULT_Kd 17.13
 
 #endif // PIDTEMP
 
